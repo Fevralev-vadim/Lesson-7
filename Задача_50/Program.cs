4a8fa3a791e70e81a4f997f3d90641dcd0a9bebc
Vadim Fevralev <fevralev.vadim@mail.ru>
2022-12-05 11:19:06 +0300

    решили задачу 50

---
+++ Задача_50/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 50: Напишите программу, которая на вход
+// принимает позиции элемента в двумерном массиве, и
+// возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+bool SearchNumberInArray(int[,] array, int findNumber)
+{
+
+    bool isFound = false;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == findNumber)
+            {
+                Console.Write((isFound ? "" : "Найдено:\n"));
+                Console.WriteLine($"[{i},{j}]");
+                isFound = true;
+            }
+        }
+    }
+    return isFound;
+}
+
+
+
+
+int[,] myTestArray = { { 1, 4, 7, 2 },
+                       { 5, 9, 2, 3 },
+                       { 8, 4, 2, 4}};
+
+Console.WriteLine("Введите число для поиска: ");
+int findNum = int.Parse(Console.ReadLine());
+bool resultSearch = SearchNumberInArray(myTestArray, findNum);
+if (!resultSearch)
+{
+    Console.WriteLine($"Элемент со значением {findNum} не найдены");
+}
+
